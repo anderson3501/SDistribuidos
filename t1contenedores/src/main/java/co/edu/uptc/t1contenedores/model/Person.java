9da97313538a60024bee0753a40266c16106b9b7
anderson3501 <116132709+anderson3501@users.noreply.github.com>
2024-03-18 21:51:07 -0500

    Add files via upload

---
+++ t1contenedores/src/main/java/co/edu/uptc/t1contenedores/model/Person.java
@@ -0,0 +1,12 @@
+package co.edu.uptc.t1contenedores.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Person {
+    private String name;
+    private String lastName;
+
+}
