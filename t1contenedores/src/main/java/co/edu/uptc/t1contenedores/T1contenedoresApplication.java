f79ba029146c4efe206ebfc522974f05eba2c901
anderson3501 <116132709+anderson3501@users.noreply.github.com>
2024-04-09 14:55:24 -0500

    Add files via upload

---
+++ t1contenedores/src/main/java/co/edu/uptc/t1contenedores/T1contenedoresApplication.java
@@ -0,0 +1,15 @@
+package co.edu.uptc.t1contenedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class T1contenedoresApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1contenedoresApplication.class, args);
+	}
+
+}
