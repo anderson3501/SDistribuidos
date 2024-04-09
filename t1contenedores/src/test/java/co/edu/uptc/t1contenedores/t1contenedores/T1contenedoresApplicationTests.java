e398f927b87751d455a96ce8afb399c97637b892
anderson3501 <116132709+anderson3501@users.noreply.github.com>
2024-04-09 14:00:40 -0500

    Add files via upload

---
+++ t1contenedores/src/test/java/co/edu/uptc/t1contenedores/t1contenedores/T1contenedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.t1contenedores.t1contenedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1contenedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
